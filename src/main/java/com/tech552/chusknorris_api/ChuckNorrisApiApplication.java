80757384b3a907e76697b49af604ff5e3e2ddbfa
TreyComic <trey.greene1153@gmail.com>
2024-06-07 12:50:05 -0400

    first commit

--- src/main/java/com/tech552/chusknorris_api/ChuckNorrisApiApplication.java
+++ src/main/java/com/tech552/chusknorris_api/ChuckNorrisApiApplication.java
@@ -8,11 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-public class ChuckNorrisApiApplication {
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+@SpringBootApplication
+@EnableScheduling
+public class ChuckNorrisApiApplication {
 	private static final Logger log = LoggerFactory.getLogger(ChuckNorrisApiApplication.class);
 
 	public static void main(String[] args) {
@@ -27,11 +34,21 @@ public class ChuckNorrisApiApplication {
 	@Bean
 	@Profile("!test")
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Joke joke = restTemplate.getForObject("https://api.chucknorris.io/jokes/random", Joke.class);
-			log.info(joke.toString());
-		};
+		ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1); //creating a schedule executor service Provides tools to set our times
+		scheduler.scheduleAtFixedRate(() -> {
+			Joke quote = restTemplate.getForObject("https://api.chucknorris.io/jokes/random", Joke.class); //fetch joke
+			log.info(quote.toString()); //log out fetched joke
+		}, 0, 5000, TimeUnit.MILLISECONDS); // no delay but a period of 5 and set the unit of time to miliseconds
+		return args -> {};
 	}
+
+//	@Bean
+//	@Profile("!test")
+//	@Scheduled(fixedRate = 5000)
+//	public void fetchChuckNorrisJoke(RestTemplate restTemplate) {
+//		Joke joke = restTemplate.getForObject("https://api.chucknorris.io/jokes/random", Joke.class);
+//		log.info(joke.getValue());
+//	}
 
 
 
--- src/main/java/com/tech552/chusknorris_api/Task/First.java
+++ src/main/java/com/tech552/chusknorris_api/Task/First.java
@@ -1,2 +1,17 @@
-package com.tech552.chusknorris_api.Task;public class First {
+package com.tech552.chusknorris_api.Task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+@Component
+public class First {
+
+    //private static final Logger log = LoggerFactory.getLogger(First.class);
+    //@Scheduled(fixedRate = 5000)
+    //public void performTask() {
+        //log.info("Performing task @ {}", Instant.now());
+    //}
 }
